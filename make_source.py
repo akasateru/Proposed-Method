8df220e7e08784dbae79d971d8b1184c52f576e9
akasateru <akasateru@gmail.com>
2021-01-08 14:26:38 +0000

    Proposed method 3.0

--- make_source.py
+++ make_source.py
@@ -92,22 +92,22 @@ def dbpedia():
             text_list_dbpedia.append(text)
 
     # #test.csv
-    # with open('../data'+os.sep+'dbpedia'+os.sep+'dbpedia_csv'+os.sep+'test.csv','r',encoding='utf-8') as f:
-    #     reader = csv.reader(f)
-    #     l = len(list(reader))
-    #     f.seek(0)
-    #     for row in tqdm(reader,total=l):
-    #         text_stock = []
-    #         text = row[2][1:].replace('(',')').split(')')
-    #         for i,t in enumerate(text):
-    #             if i % 2 == 0:
-    #                 text_stock.append(t)
-    #         text = ''.join(text_stock)
-    #         text = chenge_text(text)
-    #         text = ' '.join([x for x in text.split(' ') if x not in row[1].split(' ')])
-    #         text = text.replace('  ',' ')
-    #         vec_list_dbpedia.append(docvec(text))
-    #         text_list_dbpedia.append(text)
+    with open('../data'+os.sep+'dbpedia'+os.sep+'dbpedia_csv'+os.sep+'test.csv','r',encoding='utf-8') as f:
+        reader = csv.reader(f)
+        l = len(list(reader))
+        f.seek(0)
+        for row in tqdm(reader,total=l):
+            text_stock = []
+            text = row[2][1:].replace('(',')').split(')')
+            for i,t in enumerate(text):
+                if i % 2 == 0:
+                    text_stock.append(t)
+            text = ''.join(text_stock)
+            text = chenge_text(text)
+            text = ' '.join([x for x in text.split(' ') if x not in row[1].split(' ')])
+            text = text.replace('  ',' ')
+            vec_list_dbpedia.append(docvec(text))
+            text_list_dbpedia.append(text)
     return vec_list_dbpedia, text_list_dbpedia
     
 def reuter():
