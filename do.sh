0b7de4b1123517c3835ac2a8c1bb790555ac4787
akasateru <akasateru@gmail.com>
2021-01-10 08:13:19 +0000

    Proposed method 4.1

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 set -e
-python make_classcsv.py
-python make_source.py
+# python make_classcsv.py
+# python make_source.py
 python choice_traindata.py
 python make_dataset.py
 python textcnn_train.py
